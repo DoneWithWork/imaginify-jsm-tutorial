45b0ba705e1756b57cce4775281cb304e0d68044
DoneWithWork <fingfingsavage@gmail.com>
2024-05-03 08:44:56 +0800

    initial commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddTransformationtypePage = () => {
+  return <div></div>;
+};
+
+export default AddTransformationtypePage;
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,55 @@
+import { Schema, model, models } from "mongoose";
+
+//clerkid, email,username,photo,firstName,lastName, planId,creditbalance,createdAt,updatedAt
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  planId: {
+    type: String,
+
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
+});
+interface IUser {
+  clerkId: string;
+  email: string;
+  username: string;
+  photo: string;
+  firstName: string;
+  lastName: string;
+  planId: string;
+  creditBalance: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+const User = models?.User || model("User", UserSchema);
+export default User;
